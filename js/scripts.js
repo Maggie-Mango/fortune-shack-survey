00e73f3e91eaafb52c0b3b79e85439c8ae6e047b
Maggie S <Maggiesaldivia@gmail.com>
2021-02-04 21:12:39 -0800

    add result when user chooses that they don't want to take the survey.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,20 @@
 //user interface logic
 $(document).ready(function() {
   $("button.enter").click(function(event) {
-  $("#fortune-survey").show();
+    $("#fortune-survey").show();
+    $("#shack").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("button.exit").click(function(event) {
+    $("#intro").hide();
+    $("#leave").show();
+  });
+});
+
+$(submit).ready(function() {
+  $("button.exit").click(function(event) {
+
   });
 });
